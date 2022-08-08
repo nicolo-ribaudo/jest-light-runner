cc95962333831f5f40be2c03543313d5a19487dd
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2022-08-08 15:02:18 +0200

    Updates

--- src/worker-runner.js
+++ src/worker-runner.js
@@ -45,6 +45,8 @@ const initialSetup = once(async projectConfig => {
     const { default: setup } = await import(pathToFileURL(setupFile));
     if (typeof setup === "function") await setup();
   }
+
+  return snapshot.getSerializers().slice();
 });
 
 export default async function run({
@@ -53,8 +55,7 @@ export default async function run({
   testNamePattern,
   port,
 }) {
-  await initialSetup(test.context.config);
-  const globalSnapshotSerializers = [...snapshot.getSerializers()];
+  const projectSnapshotSerializers = await initialSetup(test.context.config);
 
   port.postMessage("start");
 
@@ -91,12 +92,10 @@ export default async function run({
     frame.file = fileURLToPath(frame.file);
   });
   snapshotState.save();
-  const snapshotSerializers = snapshot.getSerializers();
-  snapshotSerializers.splice(
-    0,
-    snapshotSerializers.length,
-    ...globalSnapshotSerializers
-  );
+
+  // Restore the project-level serializers, so that serializers
+  // installed by one test file don't leak to other files.
+  arrayReplace(snapshot.getSerializers(), projectSnapshotSerializers);
 
   return toTestResult(stats, results, test);
 }
@@ -312,4 +311,8 @@ function once(fn) {
     result = fn.apply(this, arguments);
     return result;
   };
+}
+
+function arrayReplace(array, replacement) {
+  array.splice(0, array.length, ...replacement);
 }
