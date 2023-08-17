7c26bb40e4c299fc9652f34e64f0c0aae7752f14
Sigurd Schoeler <sschoeler99@gmail.com>
2023-08-17 14:56:35 +0200

    Remove last 3 lines of stack trace
    These are always jest-light-runner internals

--- src/worker-runner.js
+++ src/worker-runner.js
@@ -333,7 +333,7 @@ function failureToString(test) {
   return (
     test.ancestors.concat(test.title).join(" > ") +
     "\n" +
-    test.errors.map(error => error.toString().replace(/^/gm, "    ")).join("\n") +
+    test.errors.map(error => error.stack.split('\n').slice(0, -3).join('\n').replace(/^/gm, "    ")).join("\n") +
     "\n"
   );
 }
