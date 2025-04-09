fb06f9f39e8eec41f51493208d87b246a80236f6
fisker Cheung <lionkay@gmail.com>
2025-04-09 18:41:24 +0800

    Support run in `child_process` (#87)

--- src/index.js
+++ src/index.js
@@ -1,70 +1,93 @@
 import Tinypool from "tinypool";
 import supportsColor from "supports-color";
 import { MessageChannel } from "worker_threads";
+import pMap from "p-map";
 
 /** @typedef {import("@jest/test-result").Test} Test */
 
-export default class LightRunner {
-  // TODO: Use real private fields when we drop support for Node.js v12
-  _config;
-  _pool;
+const createRunner = ({ runtime = "worker_threads" } = {}) =>
+  class LightRunner {
+    // TODO: Use real private fields when we drop support for Node.js v12
+    _config;
+    _pool;
+    _isProcessRunner = runtime === "child_process";
+    _runInBand = false;
 
-  constructor(config) {
-    this._config = config;
+    constructor(config) {
+      this._config = config;
 
-    // Jest's logic to decide when to spawn workers and when to run in the
-    // main thread is quite complex:
-    //  https://github.com/facebook/jest/blob/5183c1/packages/jest-core/src/testSchedulerHelper.ts#L13
-    // We will only run in the main thread when `maxWorkers` is 1.
-    // It's always 1 when using the `--runInBand` option.
-    // This is so that the tests shares the same global context as Jest only
-    // when explicitly required, to prevent them from accidentally interferring
-    // with the test runner. Jest's default runner does not have this problem
-    // because it isolates every test in a vm.Context.
-    const { maxWorkers } = config;
-    const runInBand = maxWorkers === 1;
+      // Jest's logic to decide when to spawn workers and when to run in the
+      // main thread is quite complex:
+      //  https://github.com/facebook/jest/blob/5183c1/packages/jest-core/src/testSchedulerHelper.ts#L13
+      // We will only run in the main thread when `maxWorkers` is 1.
+      // It's always 1 when using the `--runInBand` option.
+      // This is so that the tests shares the same global context as Jest only
+      // when explicitly required, to prevent them from accidentally interferring
+      // with the test runner. Jest's default runner does not have this problem
+      // because it isolates every test in a vm.Context.
+      const { maxWorkers } = config;
+      const runInBand = maxWorkers === 1;
 
-    this._pool = new (runInBand ? InBandTinypool : Tinypool)({
-      filename: new URL("./worker-runner.js", import.meta.url).href,
-      maxThreads: maxWorkers,
-      env: {
-        // Workers don't have a tty; we whant them to inherit
-        // the color support level from the main thread.
-        FORCE_COLOR: supportsColor.stdout.level,
-        ...process.env,
-      },
-    });
-  }
+      this._runInBand = runInBand;
+      this._pool = new (runInBand ? InBandTinypool : Tinypool)({
+        filename: new URL("./worker-runner.js", import.meta.url).href,
+        runtime,
+        maxThreads: maxWorkers,
+        env: {
+          // Workers don't have a tty; we whant them to inherit
+          // the color support level from the main thread.
+          FORCE_COLOR: supportsColor.stdout.level,
+          ...process.env,
+        },
+      });
+    }
 
-  /**
-   * @param {Array<Test>} tests
-   * @param {*} watcher
-   * @param {*} onStart
-   * @param {*} onResult
-   * @param {*} onFailure
-   */
-  runTests(tests, watcher, onStart, onResult, onFailure) {
-    const { updateSnapshot, testNamePattern } = this._config;
+    /**
+     * @param {Array<Test>} tests
+     * @param {*} watcher
+     * @param {*} onStart
+     * @param {*} onResult
+     * @param {*} onFailure
+     */
+    runTests(tests, watcher, onStart, onResult, onFailure) {
+      const { updateSnapshot, testNamePattern, maxWorkers } = this._config;
 
-    return Promise.all(
-      tests.map(test => {
-        const mc = new MessageChannel();
-        mc.port2.onmessage = () => onStart(test);
-        mc.port2.unref();
+      if (!this._runInBand && this._isProcessRunner) {
+        return pMap(
+          tests,
+          test => {
+            onStart(test);
 
-        return this._pool
-          .run(
-            { test, updateSnapshot, testNamePattern, port: mc.port1 },
-            { transferList: [mc.port1] }
-          )
-          .then(
-            result => void onResult(test, result),
-            error => void onFailure(test, error)
-          );
-      })
-    );
-  }
-}
+            return this._pool
+              .run({ test, updateSnapshot, testNamePattern })
+              .then(
+                result => void onResult(test, result),
+                error => void onFailure(test, error),
+              );
+          },
+          { concurrency: maxWorkers },
+        );
+      }
+
+      return Promise.all(
+        tests.map(test => {
+          const mc = new MessageChannel();
+          mc.port2.onmessage = () => onStart(test);
+          mc.port2.unref();
+
+          return this._pool
+            .run(
+              { test, updateSnapshot, testNamePattern, port: mc.port1 },
+              { transferList: [mc.port1] },
+            )
+            .then(
+              result => void onResult(test, result),
+              error => void onFailure(test, error),
+            );
+        }),
+      );
+    }
+  };
 
 // Exposes an API similar to Tinypool, but it uses dynamic import()
 // rather than worker_threads.
@@ -104,3 +127,6 @@ class InBandTinypool {
     }
   }
 }
+
+export default createRunner();
+export { createRunner };
